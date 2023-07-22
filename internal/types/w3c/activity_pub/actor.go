8d13fab589fe26aaf39d32d249c9dc35281c8a45
Grady Saccullo <gradys.dev@gmail.com>
2023-07-21 22:28:47 -0700

    fix: remove import from old activity streams folder

--- internal/types/w3c/activity_pub/actor.go
+++ internal/types/w3c/activity_pub/actor.go
@@ -1,11 +1,6 @@
 package activityPub
 
-import (
-	activityStream "github.com/Grady-Saccullo/activity-pub-go/internal/types/w3c/activity_streams/_old_"
-)
-
 type Actor struct {
-	activityStream.Actor
 	PropertyInbox
 	PropertyOutbox
 	PropertyFollowing
